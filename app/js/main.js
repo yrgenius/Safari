4c9ce893ff25caf4971ea223fca24156bd29f8fe
Anderson <yrgenius80@gmail.com>
2021-10-10 15:06:22 +0300

    added slick-slider

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,10 @@ $(function() {
 		fixedContentPos: false
 	});
 
-	$('.gallery__list').slick();
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-prev"><img class="gallery__prev" src="../images/main/prev.svg" alt="Назад"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img class="gallery__next" src="../images/main/next.svg" alt="Вперед"></button>'
+		
+	});
 
 });
