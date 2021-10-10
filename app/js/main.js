92f1d470be472aca61278ea1d24f85cebf53cfaf
Anderson <yrgenius80@gmail.com>
2021-10-10 14:27:01 +0300

    start turistic site

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('.about__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	$('.gallery__list').slick();
+
+});
